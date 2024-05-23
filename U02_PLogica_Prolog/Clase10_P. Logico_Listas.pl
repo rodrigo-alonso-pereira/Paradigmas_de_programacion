0fe535a14146d192be7a4cfe5df99e6291f405fc
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-05-22 21:51:07 -0400

    Upadate Clase 10 listas prolog

--- U02_PLogica_Prolog/Clase10_P. Logico_Listas.pl
+++ U02_PLogica_Prolog/Clase10_P. Logico_Listas.pl
@@ -1,4 +1,3 @@
-
 % Prolog
 
 % Pertenece un elemento a una lista
@@ -8,19 +7,39 @@
 %pertenece(1, [2]). -> false OK
 %pertenece(1, []). -> false OK
 
+% Pertenece de 1 elemento.
+pertenece(Elemento, [Elemento]).
+% pertenece(1, [1]).
+%s true
+
 %pertenece(1, [1,2]). -> OK
 %pertenece(1, [2,1]).
 
-%pertenece(Elemento, [Elemento | _]).
+% pertenece(Elemento, [Elemento | _]).
 
 % [1,2] -> Resto: [2]
 % [1,2] -> [Elemento | _]
 % [2] -> [2, []]
 % asi es como se recorre una lista -> FOR
+
+% Pertenece de n elementos.
 pertenece(Elemento, [Elemento|_]).
-    
+% pertenece(1, [1,2]).
+%s true
+%s false
+
+% Recursion 
 pertenece(Elemento, [_ |Resto ]) :-
 	pertenece(Elemento, Resto).
+% pertenece("Hola", [1,2,3,4,5,"Hola"]).
+%s true
+% pertenece([1,2,3], [[1,2,3,4,5,"Hola"], [1,2,3,4], [1,2,3]]).
+%s true
+% pertenece(Variable, [1,2,"Hola",[3,4,5]]).
+%s Variable = 1
+%s Variable = 2
+%s Variable = "Hola"
+%s Variable = [3, 4, 5]
 
 estacion(Nombre, Tiempo, [Nombre, Tiempo]).
 
