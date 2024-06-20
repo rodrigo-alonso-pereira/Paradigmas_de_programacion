8dc5d424654ac73183a16a31abb888ce9688e901
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-06-20 19:17:00 -0400

    Inferencia en variables

--- U03_POO_Java/Clases_POO_paradigmas/src/main/java/cl/usach/example/Main.java
+++ U03_POO_Java/Clases_POO_paradigmas/src/main/java/cl/usach/example/Main.java
@@ -3,6 +3,16 @@ package cl.usach.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Definicion de una variable
+        String valor = "Hola Mundo!";
+        int miNumero = 1;
+        double miOtroNUmero = 1.5;
+
+        //Nuevas versiones solo usar var para declarar variable
+        //Inferencia
+        var valor2 = "Hola Mundo 2!";
+        var miNumero2 = 1;
+        var miOtroNUmero2 = 1.5;
+        System.out.println(valor2);
     }
 }
